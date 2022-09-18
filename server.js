005711288216f546e6159f42baf971b7b7506ea7
dretekin <dretekin@gmail.com>
2022-09-18 10:36:15 -0400

    Adding middlewares

--- server.js
+++ server.js
@@ -1,15 +1,36 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const port = process.env.PORT || 3000
+const express = require("express");
+const app = express();
+const path = require("path");
+const { logger } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+const port = process.env.PORT || 3000;
 
-app.use('/', express.static(path.join(__dirname, '/public')))
-app.use('/', express.static(path.join(__dirname, '/public')))
+app.use(logger);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use("/", require("./routes/root"));
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 not found");
+  }
+});
+
+app.use(errorHandler);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
